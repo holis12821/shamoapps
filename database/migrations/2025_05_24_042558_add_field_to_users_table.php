495045b6ee34e24846779a07bdd8aeaaaec6839b
Nurholis <nur.kholis@ottencoffee.co.id>
2025-05-24 15:24:30 +0700

    setup database, create several table on migration

--- database/migrations/2025_05_24_042558_add_field_to_users_table.php
+++ database/migrations/2025_05_24_042558_add_field_to_users_table.php
@@ -12,7 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('roles')
+                ->after('email')
+                ->default('USER');
+            $table->string('phone')
+                ->after('email')
+                ->nullable();
+            $table->string('username')
+                ->after('email')
+                ->nullable();
         });
     }
 
@@ -22,7 +30,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('roles');
+            $table->dropColumn('phone');
+            $table->dropColumn('username');
         });
     }
 };
