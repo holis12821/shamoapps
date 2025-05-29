b7974c484c562f8095820597faa579743a54295e
Nurholis <nur.kholis@ottencoffee.co.id>
2025-05-29 16:13:41 +0700

    Fix setup laravel

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  ...$guards
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, ...$guards)
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guards) {
+            if (Auth::guard($guards)->check()) {
+                // Redirect to the home page if the user is authenticated
+                return redirect(RouteServiceProvider::HOME);
+            }
+        }
+
+        return $next($request);
+    }
+}
