ee64a15a457670475c65668fc858778127d68541
Nurholis <nur.kholis@ottencoffee.co.id>
2025-06-02 00:47:57 +0700

    Fix Image logo at login and register

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('/nike-logo.svg') }}" alt="Logo" class="w-60 h-60 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 xl:w-32 xl:h-32 object-cover rounded">
+    <img src="/nike-logo.svg" alt="Logo" style="height: 156px; " />
 </a>
